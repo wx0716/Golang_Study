521e621e34ba99209287b1e02dc84bd0e657eb41
wx <wx071611@icloud.com>
2024-02-16 22:42:47 +0800

    Create 实例5.go

---
+++ Golang项目文件/实例5.go
@@ -0,0 +1,5 @@
+// Package superman implements methods for saving the world.
+//
+// Experience has shown that a small number of procedures can prove
+// helpful when attempting to save the world.
+package main
