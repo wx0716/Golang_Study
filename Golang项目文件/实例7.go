f78f27377128df0cad736543bd8bc1c711dcf5e9
wx <wx071611@icloud.com>
2024-03-07 20:39:31 +0800

    update

---
+++ Golang项目文件/实例7.go
@@ -0,0 +1,24 @@
+const Ln2 = 0.693147180559945309417232121458\
+			176568075500134360255254120680009
+const Log2E = 1/Ln2 // this is a precise reciprocal
+const Billion = 1e9 // float constant
+const hardEight = (1 << 100) >> 97
+
+const beef, two, c = "eat", 2, "veg"
+const Monday, Tuesday, Wednesday, Thursday, Friday, Saturday = 1, 2, 3, 4, 5, 6
+const (
+	Monday, Tuesday, Wednesday = 1, 2, 3
+	Thursday, Friday, Saturday = 4, 5, 6
+)
+
+const (
+	Unknown = 0
+	Female = 1
+	Male = 2
+)
+
+const (
+	a = iota
+	b = iota
+	c = iota
+)
