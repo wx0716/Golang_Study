5936f79a5c2848bdad417920d8ee419ee88d183a
wx <wx071611@icloud.com>
2024-03-18 21:32:26 +0800

    push

--- 实例10.go
+++ 实例10.go
@@ -3,3 +3,15 @@ var a int = 15
 var i = 5
 var b bool = false
 var str string = "Go says hello to the world!"
+
+var a = 15
+var b = false
+var str = "Go says hello to the world!"
+
+var (
+	a = 15
+	b = false
+	str = "Go says hello to the world!"
+	numShips = 50
+	city string
+)
